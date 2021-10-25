8be8d1d0de0b5d801005bb0417fbd3977861913f
Daybrush <daybrush@gmail.com>
2021-10-25 22:23:35 +0900

    chore: Release 1.0.3

---
+++ dist/dragscroll.esm.js
@@ -0,0 +1,202 @@
+/*
+Copyright (c) 2019 Daybrush
+name: @scena/dragscroll
+license: MIT
+author: Daybrush
+repository: git+https://github.com/daybrush/dragscroll.git
+version: 1.0.3
+*/
+import EventEmitter from '@scena/event-emitter';
+import { now } from '@daybrush/utils';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+/* global Reflect, Promise */
+var extendStatics = function (d, b) {
+  extendStatics = Object.setPrototypeOf || {
+    __proto__: []
+  } instanceof Array && function (d, b) {
+    d.__proto__ = b;
+  } || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+  };
+
+  return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+  extendStatics(d, b);
+
+  function __() {
+    this.constructor = d;
+  }
+
+  d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+function getDefaultScrollPosition(e) {
+  var container = e.container;
+
+  if (container === document.body) {
+    return [container.scrollLeft || document.documentElement.scrollLeft, container.scrollTop || document.documentElement.scrollTop];
+  }
+
+  return [container.scrollLeft, container.scrollTop];
+}
+
+var DragScroll =
+/*#__PURE__*/
+function (_super) {
+  __extends(DragScroll, _super);
+
+  function DragScroll() {
+    var _this = _super !== null && _super.apply(this, arguments) || this;
+
+    _this.startRect = null;
+    _this.startPos = [];
+    _this.prevTime = 0;
+    _this.timer = 0;
+    return _this;
+  }
+
+  var __proto = DragScroll.prototype;
+
+  __proto.dragStart = function (e, options) {
+    var container = options.container;
+    var top = 0;
+    var left = 0;
+    var width = 0;
+    var height = 0;
+
+    if (container === document.body) {
+      width = window.innerWidth;
+      height = window.innerHeight;
+    } else {
+      var rect = container.getBoundingClientRect();
+      top = rect.top;
+      left = rect.left;
+      width = rect.width;
+      height = rect.height;
+    }
+
+    this.startPos = [e.clientX, e.clientY];
+    this.startRect = {
+      top: top,
+      left: left,
+      width: width,
+      height: height
+    };
+  };
+
+  __proto.drag = function (e, options) {
+    var _this = this;
+
+    var clientX = e.clientX,
+        clientY = e.clientY;
+    var container = options.container,
+        _a = options.threshold,
+        threshold = _a === void 0 ? 0 : _a,
+        _b = options.throttleTime,
+        throttleTime = _b === void 0 ? 0 : _b,
+        _c = options.getScrollPosition,
+        getScrollPosition = _c === void 0 ? getDefaultScrollPosition : _c;
+
+    var _d = this,
+        startRect = _d.startRect,
+        startPos = _d.startPos;
+
+    var nowTime = now();
+    var distTime = Math.max(throttleTime + this.prevTime - nowTime, 0);
+    var direction = [0, 0];
+
+    if (startRect.top > clientY - threshold) {
+      if (startPos[1] > startRect.top || clientY < startPos[1]) {
+        direction[1] = -1;
+      }
+    } else if (startRect.top + startRect.height < clientY + threshold) {
+      if (startPos[1] < startRect.top + startRect.height || clientY > startPos[1]) {
+        direction[1] = 1;
+      }
+    }
+
+    if (startRect.left > clientX - threshold) {
+      if (startPos[0] > startRect.left || clientX < startPos[0]) {
+        direction[0] = -1;
+      }
+    } else if (startRect.left + startRect.width < clientX + threshold) {
+      if (startPos[0] < startRect.left + startRect.width || clientX > startPos[0]) {
+        direction[0] = 1;
+      }
+    }
+
+    clearTimeout(this.timer);
+
+    if (!direction[0] && !direction[1]) {
+      return false;
+    }
+
+    if (distTime > 0) {
+      this.timer = window.setTimeout(function () {
+        _this.drag(e, options);
+      }, distTime);
+      return false;
+    }
+
+    this.prevTime = nowTime;
+    var prevPos = getScrollPosition({
+      container: container,
+      direction: direction
+    });
+    this.trigger("scroll", {
+      container: container,
+      direction: direction,
+      inputEvent: e
+    });
+    var nextPos = getScrollPosition({
+      container: container,
+      direction: direction
+    });
+    var offsetX = nextPos[0] - prevPos[0];
+    var offsetY = nextPos[1] - prevPos[1];
+
+    if (!offsetX && !offsetY) {
+      return false;
+    }
+
+    this.trigger("move", {
+      offsetX: direction[0] ? offsetX : 0,
+      offsetY: direction[1] ? offsetY : 0,
+      inputEvent: e
+    });
+
+    if (throttleTime) {
+      this.timer = window.setTimeout(function () {
+        _this.drag(e, options);
+      }, throttleTime);
+    }
+
+    return true;
+  };
+
+  __proto.dragEnd = function () {
+    clearTimeout(this.timer);
+  };
+
+  return DragScroll;
+}(EventEmitter);
+
+export default DragScroll;
+//# sourceMappingURL=dragscroll.esm.js.map
---
+++ dist/dragscroll.js
@@ -0,0 +1,574 @@
+/*
+Copyright (c) 2019 Daybrush
+name: @scena/dragscroll
+license: MIT
+author: Daybrush
+repository: git+https://github.com/daybrush/dragscroll.git
+version: 1.0.3
+*/
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = global || self, global.DragScroll = factory());
+}(this, (function () { 'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+
+    /* global Reflect, Promise */
+    var extendStatics = function (d, b) {
+      extendStatics = Object.setPrototypeOf || {
+        __proto__: []
+      } instanceof Array && function (d, b) {
+        d.__proto__ = b;
+      } || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+      };
+
+      return extendStatics(d, b);
+    };
+
+    function __extends(d, b) {
+      extendStatics(d, b);
+
+      function __() {
+        this.constructor = d;
+      }
+
+      d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    }
+
+    /*
+    Copyright (c) 2018 Daybrush
+    @name: @daybrush/utils
+    license: MIT
+    author: Daybrush
+    repository: https://github.com/daybrush/utils
+    @version 1.0.0
+    */
+    /**
+    * get string "object"
+    * @memberof Consts
+    * @example
+    import {OBJECT} from "@daybrush/utils";
+
+    console.log(OBJECT); // "object"
+    */
+
+    var OBJECT = "object";
+    /**
+    * Check the type that the value is object.
+    * @memberof Utils
+    * @param {string} value - Value to check the type
+    * @return {} true if the type is correct, false otherwise
+    * @example
+    import {isObject} from "@daybrush/utils";
+
+    console.log(isObject({})); // true
+    console.log(isObject(undefined)); // false
+    console.log(isObject("")); // false
+    console.log(isObject(null)); // false
+    */
+
+    function isObject(value) {
+      return value && typeof value === OBJECT;
+    }
+    /**
+    * Returns the index of the first element in the array that satisfies the provided testing function.
+    * @function
+    * @memberof CrossBrowser
+    * @param - The array `findIndex` was called upon.
+    * @param - A function to execute on each value in the array until the function returns true, indicating that the satisfying element was found.
+    * @param - Returns defaultIndex if not found by the function.
+    * @example
+    import { findIndex } from "@daybrush/utils";
+
+    findIndex([{a: 1}, {a: 2}, {a: 3}, {a: 4}], ({ a }) => a === 2); // 1
+    */
+
+    function findIndex(arr, callback, defaultIndex) {
+      if (defaultIndex === void 0) {
+        defaultIndex = -1;
+      }
+
+      var length = arr.length;
+
+      for (var i = 0; i < length; ++i) {
+        if (callback(arr[i], i, arr)) {
+          return i;
+        }
+      }
+
+      return defaultIndex;
+    }
+
+    /*
+    Copyright (c) 2019 Daybrush
+    name: @scena/event-emitter
+    license: MIT
+    author: Daybrush
+    repository: git+https://github.com/daybrush/gesture.git
+    version: 1.0.2
+    */
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+    var __assign = function () {
+      __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+
+          for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+
+        return t;
+      };
+
+      return __assign.apply(this, arguments);
+    };
+    function __spreadArrays() {
+      for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+
+      for (var r = Array(s), k = 0, i = 0; i < il; i++) for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++) r[k] = a[j];
+
+      return r;
+    }
+
+    /**
+     * Implement EventEmitter on object or component.
+     */
+
+    var EventEmitter =
+    /*#__PURE__*/
+    function () {
+      function EventEmitter() {
+        this._events = {};
+      }
+      /**
+       * Add a listener to the registered event.
+       * @param - Name of the event to be added
+       * @param - listener function of the event to be added
+       * @example
+       * import EventEmitter from "@scena/event-emitter";
+       * cosnt emitter = new EventEmitter();
+       *
+       * // Add listener in "a" event
+       * emitter.on("a", () => {
+       * });
+       * // Add listeners
+       * emitter.on({
+       *  a: () => {},
+       *  b: () => {},
+       * });
+       */
+
+
+      var __proto = EventEmitter.prototype;
+
+      __proto.on = function (eventName, listener) {
+        if (isObject(eventName)) {
+          for (var name in eventName) {
+            this.on(name, eventName[name]);
+          }
+        } else {
+          this._addEvent(eventName, listener, {});
+        }
+
+        return this;
+      };
+      /**
+       * Remove listeners registered in the event target.
+       * @param - Name of the event to be removed
+       * @param - listener function of the event to be removed
+       * @example
+       * import EventEmitter from "@scena/event-emitter";
+       * cosnt emitter = new EventEmitter();
+       *
+       * // Remove all listeners.
+       * emitter.off();
+       *
+       * // Remove all listeners in "A" event.
+       * emitter.off("a");
+       *
+       *
+       * // Remove "listener" listener in "a" event.
+       * emitter.off("a", listener);
+       */
+
+
+      __proto.off = function (eventName, listener) {
+        if (!eventName) {
+          this._events = {};
+        } else if (isObject(eventName)) {
+          for (var name in eventName) {
+            this.off(name);
+          }
+        } else if (!listener) {
+          this._events[eventName] = [];
+        } else {
+          var events = this._events[eventName];
+
+          if (events) {
+            var index = findIndex(events, function (e) {
+              return e.listener === listener;
+            });
+
+            if (index > -1) {
+              events.splice(index, 1);
+            }
+          }
+        }
+
+        return this;
+      };
+      /**
+       * Add a disposable listener and Use promise to the registered event.
+       * @param - Name of the event to be added
+       * @param - disposable listener function of the event to be added
+       * @example
+       * import EventEmitter from "@scena/event-emitter";
+       * cosnt emitter = new EventEmitter();
+       *
+       * // Add a disposable listener in "a" event
+       * emitter.once("a", () => {
+       * });
+       *
+       * // Use Promise
+       * emitter.once("a").then(e => {
+       * });
+       */
+
+
+      __proto.once = function (eventName, listener) {
+        var _this = this;
+
+        if (listener) {
+          this._addEvent(eventName, listener, {
+            once: true
+          });
+        }
+
+        return new Promise(function (resolve) {
+          _this._addEvent(eventName, resolve, {
+            once: true
+          });
+        });
+      };
+      /**
+       * Fires an event to call listeners.
+       * @param - Event name
+       * @param - Event parameter
+       * @return If false, stop the event.
+       * @example
+       *
+       * import EventEmitter from "@scena/event-emitter";
+       *
+       *
+       * const emitter = new EventEmitter();
+       *
+       * emitter.on("a", e => {
+       * });
+       *
+       *
+       * emitter.emit("a", {
+       *   a: 1,
+       * });
+       */
+
+
+      __proto.emit = function (eventName, param) {
+        var _this = this;
+
+        if (param === void 0) {
+          param = {};
+        }
+
+        var events = this._events[eventName];
+
+        if (!eventName || !events) {
+          return true;
+        }
+
+        var isStop = false;
+        param.eventType = eventName;
+
+        param.stop = function () {
+          isStop = true;
+        };
+
+        param.currentTarget = this;
+
+        __spreadArrays(events).forEach(function (info) {
+          info.listener(param);
+
+          if (info.once) {
+            _this.off(eventName, info.listener);
+          }
+        });
+
+        return !isStop;
+      };
+      /**
+       * Fires an event to call listeners.
+       * @param - Event name
+       * @param - Event parameter
+       * @return If false, stop the event.
+       * @example
+       *
+       * import EventEmitter from "@scena/event-emitter";
+       *
+       *
+       * const emitter = new EventEmitter();
+       *
+       * emitter.on("a", e => {
+       * });
+       *
+       *
+       * emitter.emit("a", {
+       *   a: 1,
+       * });
+       */
+
+      /**
+      * Fires an event to call listeners.
+      * @param - Event name
+      * @param - Event parameter
+      * @return If false, stop the event.
+      * @example
+      *
+      * import EventEmitter from "@scena/event-emitter";
+      *
+      *
+      * const emitter = new EventEmitter();
+      *
+      * emitter.on("a", e => {
+      * });
+      *
+      * // emit
+      * emitter.trigger("a", {
+      *   a: 1,
+      * });
+      */
+
+
+      __proto.trigger = function (eventName, param) {
+        if (param === void 0) {
+          param = {};
+        }
+
+        return this.emit(eventName, param);
+      };
+
+      __proto._addEvent = function (eventName, listener, options) {
+        var events = this._events;
+        events[eventName] = events[eventName] || [];
+        var listeners = events[eventName];
+        listeners.push(__assign({
+          listener: listener
+        }, options));
+      };
+
+      return EventEmitter;
+    }();
+
+    /*
+    Copyright (c) 2018 Daybrush
+    @name: @daybrush/utils
+    license: MIT
+    author: Daybrush
+    repository: https://github.com/daybrush/utils
+    @version 1.6.0
+    */
+    /**
+    * Date.now() method
+    * @memberof CrossBrowser
+    * @return {number} milliseconds
+    * @example
+    import {now} from "@daybrush/utils";
+
+    console.log(now()); // 12121324241(milliseconds)
+    */
+
+    function now() {
+      return Date.now ? Date.now() : new Date().getTime();
+    }
+
+    function getDefaultScrollPosition(e) {
+      var container = e.container;
+
+      if (container === document.body) {
+        return [container.scrollLeft || document.documentElement.scrollLeft, container.scrollTop || document.documentElement.scrollTop];
+      }
+
+      return [container.scrollLeft, container.scrollTop];
+    }
+
+    var DragScroll =
+    /*#__PURE__*/
+    function (_super) {
+      __extends(DragScroll, _super);
+
+      function DragScroll() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+
+        _this.startRect = null;
+        _this.startPos = [];
+        _this.prevTime = 0;
+        _this.timer = 0;
+        return _this;
+      }
+
+      var __proto = DragScroll.prototype;
+
+      __proto.dragStart = function (e, options) {
+        var container = options.container;
+        var top = 0;
+        var left = 0;
+        var width = 0;
+        var height = 0;
+
+        if (container === document.body) {
+          width = window.innerWidth;
+          height = window.innerHeight;
+        } else {
+          var rect = container.getBoundingClientRect();
+          top = rect.top;
+          left = rect.left;
+          width = rect.width;
+          height = rect.height;
+        }
+
+        this.startPos = [e.clientX, e.clientY];
+        this.startRect = {
+          top: top,
+          left: left,
+          width: width,
+          height: height
+        };
+      };
+
+      __proto.drag = function (e, options) {
+        var _this = this;
+
+        var clientX = e.clientX,
+            clientY = e.clientY;
+        var container = options.container,
+            _a = options.threshold,
+            threshold = _a === void 0 ? 0 : _a,
+            _b = options.throttleTime,
+            throttleTime = _b === void 0 ? 0 : _b,
+            _c = options.getScrollPosition,
+            getScrollPosition = _c === void 0 ? getDefaultScrollPosition : _c;
+
+        var _d = this,
+            startRect = _d.startRect,
+            startPos = _d.startPos;
+
+        var nowTime = now();
+        var distTime = Math.max(throttleTime + this.prevTime - nowTime, 0);
+        var direction = [0, 0];
+
+        if (startRect.top > clientY - threshold) {
+          if (startPos[1] > startRect.top || clientY < startPos[1]) {
+            direction[1] = -1;
+          }
+        } else if (startRect.top + startRect.height < clientY + threshold) {
+          if (startPos[1] < startRect.top + startRect.height || clientY > startPos[1]) {
+            direction[1] = 1;
+          }
+        }
+
+        if (startRect.left > clientX - threshold) {
+          if (startPos[0] > startRect.left || clientX < startPos[0]) {
+            direction[0] = -1;
+          }
+        } else if (startRect.left + startRect.width < clientX + threshold) {
+          if (startPos[0] < startRect.left + startRect.width || clientX > startPos[0]) {
+            direction[0] = 1;
+          }
+        }
+
+        clearTimeout(this.timer);
+
+        if (!direction[0] && !direction[1]) {
+          return false;
+        }
+
+        if (distTime > 0) {
+          this.timer = window.setTimeout(function () {
+            _this.drag(e, options);
+          }, distTime);
+          return false;
+        }
+
+        this.prevTime = nowTime;
+        var prevPos = getScrollPosition({
+          container: container,
+          direction: direction
+        });
+        this.trigger("scroll", {
+          container: container,
+          direction: direction,
+          inputEvent: e
+        });
+        var nextPos = getScrollPosition({
+          container: container,
+          direction: direction
+        });
+        var offsetX = nextPos[0] - prevPos[0];
+        var offsetY = nextPos[1] - prevPos[1];
+
+        if (!offsetX && !offsetY) {
+          return false;
+        }
+
+        this.trigger("move", {
+          offsetX: direction[0] ? offsetX : 0,
+          offsetY: direction[1] ? offsetY : 0,
+          inputEvent: e
+        });
+
+        if (throttleTime) {
+          this.timer = window.setTimeout(function () {
+            _this.drag(e, options);
+          }, throttleTime);
+        }
+
+        return true;
+      };
+
+      __proto.dragEnd = function () {
+        clearTimeout(this.timer);
+      };
+
+      return DragScroll;
+    }(EventEmitter);
+
+    return DragScroll;
+
+})));
+//# sourceMappingURL=dragscroll.js.map
